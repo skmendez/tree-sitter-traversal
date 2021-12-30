e5e8f291dd72043ef7ed572e29923f8caeec773d
Sebastian Mendez <skmendez@mit.edu>
2021-12-30 15:46:38 -0500

    tiny fix

--- benches/traversal.rs
+++ benches/traversal.rs
@@ -3,11 +3,10 @@ extern crate test;
 
 use test::Bencher;
 
-use tree_sitter_traversal::{traverse_tree, Order};
-use tree_sitter::{Parser, Tree};
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
+use tree_sitter::{Parser, Tree};
+use tree_sitter_traversal::{traverse_tree, Order};
 
 fn dogfood() -> Tree {
     let mut file = File::open("./src/lib.rs").expect("src/lib.rs should exist");
@@ -15,7 +14,7 @@ fn dogfood() -> Tree {
     file.read_to_string(&mut code).unwrap();
 
     let mut parser = Parser::new();
-    let lang = tree_sitter_javascript::language();
+    let lang = tree_sitter_rust::language();
     parser
         .set_language(lang)
         .expect("Error loading Rust grammar");
@@ -26,18 +25,14 @@ fn dogfood() -> Tree {
 
 #[bench]
 fn dogfooding_preorder(b: &mut Bencher) {
-   let tree = dogfood();
+    let tree = dogfood();
 
-    b.iter(|| {
-        traverse_tree(&tree, Order::Pre).collect::<Vec<_>>()
-    });
+    b.iter(|| traverse_tree(&tree, Order::Pre).collect::<Vec<_>>());
 }
 
 #[bench]
 fn dogfooding_postorder(b: &mut Bencher) {
     let tree = dogfood();
 
-    b.iter(|| {
-        traverse_tree(&tree, Order::Post).collect::<Vec<_>>()
-    });
+    b.iter(|| traverse_tree(&tree, Order::Post).collect::<Vec<_>>());
 }
